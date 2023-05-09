262d263c686b88fdfbfb0244392416c0552e5035
Mohammed <sajidcurious9@gmail.com>
2023-05-09 07:10:10 +0530

    added required pages

---
+++ pages/mugs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const mugs = () => {
+  return <div>hgfdftghjkjhgfdfghjkl</div>;
+};
+
+export default mugs;
---
+++ pages/tshirts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const tshirts = () => {
+  return <div>tshirts</div>;
+};
+
+export default tshirts;
