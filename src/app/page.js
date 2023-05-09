5195a96e5bdc14d53f803eb4ad6a8572c49440ac
Mohammed <sajidcurious9@gmail.com>
2023-05-09 08:03:39 +0530

    added navbar and footer components in to the home page

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Footer />
+    </main>
+  );
+}
