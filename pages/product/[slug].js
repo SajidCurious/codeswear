dd84faca4c91570db469d73032a4e2cfcf5ed3db
Mohammed <sajidcurious9@gmail.com>
2023-05-09 07:37:29 +0530

    aranging the components

--- pages/product/[slug].js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Slug = () => {
-  return <div>Slug</div>;
-};
-
-export default Slug;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <Head>
+        <title>Codeswear.com - Wear the code</title>
+      </Head>
+    </div>
+  );
+};
+
+export default index;
