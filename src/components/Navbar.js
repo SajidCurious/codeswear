2f7dad8dfabeaac98390251b30e35a3d36225e08
Mohammed <sajidcurious9@gmail.com>
2023-06-19 15:45:27 +0530

    making Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="py-2 flex flex-col md:flex-row items-center">
+      <div className="logo">
+        <span className="ml-3 text-xl font-semibold">CODESWEAR.COM</span>
+      </div>
+      <div className="nav">
+        <ul className="flex">
+          <li>Tshirts</li>
+          <li>Hoodies</li>
+          <li>Stickers</li>
+          <li>Mugs</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
