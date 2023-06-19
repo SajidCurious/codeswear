7e9172997c1bffee905f06797542f800beabc8a9
Mohammed <sajidcurious9@gmail.com>
2023-06-19 16:47:27 +0530

    added pages to the Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="py-2 flex flex-col md:flex-row items-center">
+    <div className="py-2 flex flex-col md:flex-row items-center md:justify-start">
       <div className="logo">
-        <span className="ml-3 text-xl font-semibold">CODESWEAR.COM</span>
+        <span className="ml-3 text-xl font-semibold mx-5">CODESWEAR.COM</span>
       </div>
       <div className="nav">
-        <ul className="flex">
-          <li>Tshirts</li>
-          <li>Hoodies</li>
-          <li>Stickers</li>
-          <li>Mugs</li>
+        <ul className="flex space-x-2 font-bold md:text-xl">
+          <Link href={"/"}>
+            <li>Tshirts</li>
+          </Link>
+          <Link href={"/"}>
+            <li>Hoodies</li>
+          </Link>
+          <Link href={"/"}>
+            <li>Stickers</li>
+          </Link>
+          <Link href={"/"}>
+            <li>Mugs</li>
+          </Link>
         </ul>
+      </div>
+      <div className="cart absolute right-0 mx-2 ">
+        <button className="font-bold">Cart</button>
       </div>
     </div>
   );
