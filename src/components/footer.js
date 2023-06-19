bdb3cfbeb47c3fdd09e8e30ab2d2e4e9161960fa
Mohammed <sajidcurious9@gmail.com>
2023-06-19 12:38:24 +0530

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
@@ -7,7 +8,9 @@ const Footer = () => {
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <span className="ml-3 text-xl">CODESWEAR.COM</span>
+              <Link href={"/"}>
+                <span className="ml-3 text-xl">CODESWEAR.COM</span>
+              </Link>
             </a>
             <p className="mt-2 px-4 text-sm text-gray-500">
               Air plant banjo lyft occupy retro adaptogen indego
