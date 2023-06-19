a9228cae666a77011f603255953ee09b6814968b
Mohammed <sajidcurious9@gmail.com>
2023-06-19 17:15:42 +0530

    added my social profile in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -130,14 +130,14 @@ const Footer = () => {
         <div className="bg-gray-100">
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p className="text-gray-500 text-sm text-center sm:text-left">
-              © 2020 Tailblocks —
+              © 2023 Codeswear —
               <a
-                href="https://twitter.com/knyttneve"
+                href="https://twitter.com/@sajid_curious"
                 rel="noopener noreferrer"
                 className="text-gray-600 ml-1"
                 target="_blank"
               >
-                @knyttneve
+                @sajid_curious
               </a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
