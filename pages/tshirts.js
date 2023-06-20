dd1d16ebe852b772542af6bcd5ada71fd423009a
Mohammed <sajidcurious9@gmail.com>
2023-06-21 01:54:16 +0530

    edited and separated pages folder

---
+++ pages/tshirts.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import "../app/globals.css";
+import Footer from "../components/Footer";
+
+const tshirts = () => {
+  return (
+    <div>
+      <Navbar />
+      kkkkkkkkkkkkkkkk
+      <Footer />
+    </div>
+  );
+};
+
+export default tshirts;
