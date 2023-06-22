a8e8b786a147342f1bf6a5dabfc43719fe6ac140
Mohammed <sajidcurious9@gmail.com>
2023-06-22 23:35:34 +0530

    changed section to div

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -6,7 +6,7 @@ const tshirts = () => {
   return (
     <>
       <Navbar />
-      <section className="text-gray-600 body-font">
+      <div className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 mx-auto ">
             <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
@@ -155,7 +155,7 @@ const tshirts = () => {
             </div>
           </div>
         </div>
-      </section>
+      </div>
       <Footer />
     </>
   );
