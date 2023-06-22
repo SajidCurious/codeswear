86e2a4d859789ead3d6871c421c2017e0aff773a
Mohammed <sajidcurious9@gmail.com>
2023-06-22 23:34:54 +0530

    building mug page

--- pages/mugs.js
+++ pages/mugs.js
@@ -1,7 +1,164 @@
-import React from "react";
+import "../app/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const mugs = () => {
-  return <div>hgfdftghjkjhgfdfghjkl</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4 mx-auto ">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] block object-center lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear
+                </h2>
+                <p className="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] block object-center lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Neptune
+                </h2>
+                <p className="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] block object-center lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1">$18.40</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] block object-center lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] block object-center lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear
+                </h2>
+                <p className="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] block object-center lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Neptune
+                </h2>
+                <p className="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+              <a className="block object-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:m-0 h-[36vh] block object-center lg:pl-24"
+                  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._AC_UL600_FMwebp_QL65_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1">$18.40</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default mugs;
